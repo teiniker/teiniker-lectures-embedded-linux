d28b6c405f639b408e853194c2c1d4abd155430c
teiniker <egon.teiniker@gmail.com>
2026-08-25 15:54:38 +0200

    Added example.

---
+++ raspberry-pi/peripherals/digital-io/python/pwm/led_software_pwm.py
@@ -0,0 +1,14 @@
+from gpiozero import PWMLED
+from time import sleep
+
+# Software PWM: works on any pin
+led = PWMLED(17)
+
+while True:
+    # Duty cycle value ranges from 0.0 (0%) to 1.0 (100%)
+    for duty in range(0, 101, 5):
+        led.value = duty / 100.0
+        sleep(0.05)
+    for duty in range(100, -1, -5):
+        led.value = duty / 100.0
+        sleep(0.05)
